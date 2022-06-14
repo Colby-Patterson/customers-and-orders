75b565877cd4ffa5cfefdd155c882dde90cbbd6d
Colby Patterson <cpatt@byui.edu>
2022-06-14 16:38:04 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ c1 = Customer.create(name: 'John Peterson', address: '1234 River Road')
 c2 = Customer.create(name: 'Jake Johanson', address: '1234 Robin Road')
 
 c1.orders.create(product_name: 'Computer Mouse', quantity: 1, total_price: 22.99)
+c1.orders.create(product_name: 'Dog Food', quantity: 2, total_price: 51.95)
+c2.orders.create(product_name: 'Lens Cloth', quantity: 10, total_price: 10.99)

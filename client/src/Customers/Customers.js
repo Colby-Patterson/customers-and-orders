30a5afaaba67594fbf2b30bf8cb688832728f2bc
Colby Patterson <cpatt@byui.edu>
2022-06-15 14:55:40 -0600

    Get and add customer working

--- client/src/Customers/Customers.js
+++ client/src/Customers/Customers.js
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react"
-import axios from 'axios'
+import { useContext, useEffect, useState } from "react"
+// import axios from 'axios'
 import Customer from "./Customer"
+import { DataContext } from "../providers/DataProvider"
 
 const Customers = () => {
-  const [customers, setCustomers] = useState([])
+  const {customers, addCustomer} = useContext(DataContext)
+  // const [customers, setCustomers] = useState([])
   const [loading, setLoading] = useState(true)
 
-  useEffect(()=>{
-    getCustomers()
-  }, [])
+  // useEffect(()=>{
+  //   getCustomers()
+  // }, [])
 
-  const getCustomers = async () => {
-    try {
-      let res = await axios.get('/api/customers')
-      setCustomers(res.data)
-      setLoading(false)
-    } catch(err){
-      setLoading(false)
-      console.log(err)
-      alert('Error has occured in get customers')
-    }
-  }
+  // const getCustomers = async () => {
+  //   try {
+  //     let res = await axios.get('/api/customers')
+  //     setCustomers(res.data)
+  //     setLoading(false)
+  //   } catch(err){
+  //     setLoading(false)
+  //     console.log(err)
+  //     alert('Error has occured in get customers')
+  //   }
+  // }
 
   const renderCustomers = () => {
     return customers.map(c=> <Customer key={c.id} {...c} />)
@@ -30,6 +32,8 @@ const Customers = () => {
     <div>
       List of customers
       {renderCustomers()}
+      <h1>Test Buttons</h1>
+      <button onClick={()=> addCustomer({name: 'New Customer', address: '1234 New Road'})}>Add Test Customer</button>
     </div>
   )
 }
--- client/src/providers/DataProvider.js
+++ client/src/providers/DataProvider.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export const DataContext = React.createContext
+export const DataContext = React.createContext()
 
 const DataProvider = (props) => {
+  const [customers, setCustomers] = useState([])
+
+  useEffect(()=>{
+    getCustomers()
+  }, [])
+
+  const getCustomers = async () => {
+    try {
+      let res = await axios.get('/api/customers')
+      console.log(res)
+      setCustomers(res.data)
+    } catch(err){
+      console.log(err)
+      alert('Error has occured in get customers')
+    }
+  }
+
+  const addCustomer = async (customer) => {
+    try{
+      let res = await axios.post('/api/customers', customer)
+      setCustomers([res.data, ...customers])
+    } catch(err) {
+      alert('Error occured in addCustomer')
+    }
+  }
 
   return (
-    <DataContext.Provider>
+    <DataContext.Provider value={{customers, getCustomers, addCustomer}}>
       {props.children}
     </DataContext.Provider>
   )
